8cbdfea6d0d7e93563ef6bc28b4e7730901b72ac
Ilimbek Meimenbaev <ilimbek.meimanbaev00@gmail.com>
2023-04-06 20:17:17 +0600

    Added Repository N2

--- Application/Intrefaces/IRepository.cs
+++ Application/Intrefaces/IRepository.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Thesis.Domain.Entities.Common;
 
 namespace Application.Intrefaces
 {
-    internal class IRepository
+    public interface IRepository<TKey,T>
+        where T : BaseEntity<TKey>
     {
+        public Task<TKey> CreateAsync(T entity) ;
+        public Task GetAll();
+        public Task<T> GetAsyncById(T id) ;
+        public Task UpdateAsync(T entity);
+        public Task DeleteAsync(T entity);
     }
 }
--- Application/Repositories/BaseRepository.cs
+++ Application/Repositories/BaseRepository.cs
@@ -1,28 +1,53 @@
 ﻿using Application.Intrefaces;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Thesis.Domain.Entities.Common;
 using Thesis.Persistence;
 
 namespace Application.Repositories
 {
-    public class BaseRepository<T> : IRepository<T>
+    public class BaseRepository<TKey,T> : IRepository<TKey,T>
+        where T : BaseEntity<TKey>
     {
         private readonly ThesisDbContext _context;
-
-        public Task<T> CreateAsync(T entity)
+        public BaseRepository(ThesisDbContext context) => _context = context;
+        public async Task<TKey> CreateAsync(T entity)
         {
-            throw new NotImplementedException();
+           
+            if (entity == null)
+            {
+                throw new ArgumentNullException("Entity is null");
+            }
+            _context.Set<T>().Add(entity);
+            await _context.SaveChangesAsync();
+            return entity.Id;
         }
 
-        public Task DeleteAsync(T entity)
+        public async Task DeleteAsync(T entity)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException("Entity is null");
+            }
+            _context.Set<T>().ExecuteDelete();
+            await _context.SaveChangesAsync();
+        }
+        public Task GetAll()
         {
             throw new NotImplementedException();
         }
 
         public Task<T> GetAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> GetAsyncById(T id)
         {
             throw new NotImplementedException();
         }

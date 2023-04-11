e7a4f7949482a4f13de1442de3f98c090eab09b4
DaNaKi-Yeah <usupbaev2@mail.ru>
2023-04-11 17:23:28 +0600

    fix CreateAsync() in class BaseRepository

--- Application/Repositories/BaseRepository.cs
+++ Application/Repositories/BaseRepository.cs
@@ -16,6 +16,8 @@ namespace Application.Repositories
         where T : BaseEntity<TKey>
     {
         private readonly ThesisDbContext _context;
+        private readonly DbSet<T> _dbset;
+
         public BaseRepository(ThesisDbContext context)
         {
             _context = context;
@@ -23,39 +25,44 @@ namespace Application.Repositories
             {
                 throw new ArgumentNullException(nameof(T));
             }
+
+            _dbset = context.Set<T>();
         }
+
+
         public async Task<TKey> CreateAsync(T entity)
         {
-            await _context.Set<T>().AddAsync(entity);
+            var result = (await _dbset.AddAsync(entity)).Entity;
             await _context.SaveChangesAsync();
-            return entity.Id;
+            return result.Id;
         }
 
         public async Task DeleteAsync(T entity)
         {
-            _context.Set<T>().Remove(entity);
+            _dbset.remove(entity);
             await _context.SaveChangesAsync();
         }
+
         public async Task<List<T>> GetAll()
         {
-            return _context.Set<T>().ToList();
+            return _dbset.ToList();
         }
 
         public async Task<T> GetAsync(T entity)
         {
-            T result =  _context.Set<T>().FirstOrDefault(entity);
+            T result =  _dbset.FirstOrDefault(entity);
             return result;
         }
 
         public async Task<T> GetAsyncById(TKey id)
         {
-            T result = _context.Set<T>().FirstOrDefault(item => item.Id.Equals(id));
+            T result = _dbset.FirstOrDefault(item => item.Id.Equals(id));
             return result;
         }
 
         public async Task UpdateAsync(T entity)
         {
-            _context.Set<T>().Update(entity);
+            _dbset.Update(entity);
             await _context.SaveChangesAsync();
         }
     }
